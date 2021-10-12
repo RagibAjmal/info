e549435eb95c07409cb30cd539db6132045d3138
Ragib Ajmal <ragibajmal@Ragib-ka-Mac-mini.local>
2021-10-12 21:22:40 +0530

    Final design for LHD

--- src/App.js
+++ src/App.js
@@ -12,11 +12,13 @@ function App() {
         <div>
           <header >
             <p className="Heading">Hai Everyone , I am Ragib Ajmal</p>
-          
+            <iframe src="https://giphy.com/embed/B6ks3eTSxhk4EvABa6" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
             <div className="p-text-italic"> <p>I am  beginner in Computer Science Field and  </p></div>
+            <iframe src="https://giphy.com/embed/lvwHNWgF5aol5DkBXf" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
             <div className="p-text-italic"> <p>I would like to become a DEVOPS Engineers </p></div>
+            <iframe src="https://giphy.com/embed/jnKaXnx78QNRQEbNkX" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
             <div className="p-text-italic"> <p>I have knowledge in Django, Docker, Kubernetes, React </p></div>
-        
+            <iframe src="https://giphy.com/embed/eNAsjO55tPbgaor7ma" width="476" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
           </header>
         </div>
         
@@ -36,8 +38,10 @@ function App() {
             </a>    
           </div>    
         </div>
+        <iframe src="https://giphy.com/embed/PVxtphubz0HxWwwc7u" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
         <footer className="p-d-flex p-jc-center p-ai-center">
           <p>Made with  <i className="pi pi-heart" aria-hidden="true"></i> by Ragib Ajmal</p>
+          <iframe src="https://giphy.com/embed/Z63IGkT6bmiEg3vo8o" width="363" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
           <p>
             <span className="p-mr-2">Find the code on </span>
             <a
