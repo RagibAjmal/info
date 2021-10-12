3113dcff2a857ae136a1656672ba93b1163f902d
Ragib Ajmal <ragibajmal@Ragib-ka-Mac-mini.local>
2021-10-12 20:29:03 +0530

    created basic Website

--- src/App.js
+++ src/App.js
@@ -1,27 +1,58 @@
-import 'primereact/resources/themes/saga-blue/theme.css'
-import 'primereact/resources/primereact.min.css'
+import 'primereact/resources/themes/saga-green/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
 
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <div>
+          <header >
+            <p className="Heading">Hai Everyone , I am Ragib Ajmal</p>
+          
+            <div className="p-text-italic"> <p>I am  beginner in Computer Science Field and  </p></div>
+            <div className="p-text-italic"> <p>I would like to become a DEVOPS Engineers </p></div>
+            <div className="p-text-italic"> <p>I have knowledge in Django, Docker, Kubernetes, React </p></div>
+        
+          </header>
+        </div>
+        
+        <div className="p-d-flex p-jc-center p-ai-center"> You can find  me on 
+          <div>
+            <a
+              href="https://twitter.com/RagibAjmal"
+              className="p-mr-2"
+            >
+                <i className="pi pi-twitter" aria-hidden="true"></i>
+            </a>   
+            <a
+              href="https://github.com/RagibAjmal/"
+              className="p-mr-2"
+            >
+                <i className="pi pi-github" aria-hidden="true"></i>
+            </a>    
+          </div>    
+        </div>
+        <footer className="p-d-flex p-jc-center p-ai-center">
+          <p>Made with  <i className="pi pi-heart" aria-hidden="true"></i> by Ragib Ajmal</p>
+          <p>
+            <span className="p-mr-2">Find the code on </span>
+            <a
+              href="https://github.com/RagibAjmal/info"
+              className="p-mr-2"
+              aria-label="MY website repository on GitHub"
+            >
+              <i className="pi pi-github" aria-hidden="true"></i>
+            </a>
+          </p>
+        </footer>
+      </div>
     </div>
+  
+    
   );
 }
 
